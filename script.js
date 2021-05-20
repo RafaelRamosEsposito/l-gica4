6c8d236203cb52d1ddb1ccc4a130e775808a5ac1
RafaelRamosEsposito <rafaelramosesposito@gmail.com>
2021-05-20 20:27:18 -0300

    Finalizei o quarto desafio sobre lógica

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function atividade (){
+    console.log ("Desafio sobre Lógica")
+}
+atividade ()
+function nome (name){
+    console.log (`Meu nome é ${name}`)
+}
+nome("Rafael")
+function perfil(nome, idade, estilo){
+    console.log (`Meu nome é ${nome} tenho ${idade} anos e gosto de ${estilo}`)
+}
+perfil("Rafael", 36, "pagode")
+function favorito(filme, musica){
+    console.log (`Meu filme favorito é ${filme} e minha música favorita é ${musica}`)
+}
+favorito ("Em busca da Felicidade", "Deixa a vida me Levar")
+function multiplicacao (n){
+    let res = n * 3
+    console.log (res)
+}
+multiplicacao (10)
